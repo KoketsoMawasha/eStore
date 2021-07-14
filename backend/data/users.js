442de1349ef20c0c04cbf5caf5984729f5afb6d2
Koketso Mawasha <Koketsomawasha@gmail.com>
2021-07-14 03:35:42 +0200

    modelled data for Mongo

---
+++ backend/data/users.js
@@ -0,0 +1,18 @@
+const users = [
+  {
+    name: "Admin User",
+    email: "admin@example.com",
+    password: xxxx,
+    isAdmin: true,
+  },
+  {
+    name: "Anita Baker",
+    email: "anita@example.com",
+    password: xxxx,
+  },
+  {
+    name: "Richard Maponya",
+    email: "richrich@example.com",
+    password: xxxx,
+  },
+];
