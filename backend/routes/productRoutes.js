dff6abd8655169ea827b22173e2d9a9eb437f4fc
Koketso Mawasha <Koketsomawasha@gmail.com>
2021-07-20 01:10:06 +0200

    user api endpoint issue, returning error

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,39 +1,12 @@
 import express from "express";
-import asyncHandler from "express-async-handler";
 const router = express.Router();
-import Product from "../models/productModel.js";
+import {
+  getProductById,
+  getProducts,
+} from "../controllers/productController.js";
 
-//@desc fetch all products
-//@route GET /api/products
-//@access PUBLIC
+router.route("/").get(getProducts);
 
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-
-    res.json(products);
-  })
-);
-
-//@desc fetch single products
-//@route GET /api/products/:id
-//@access PUBLIC
-
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product not found");
-    }
-
-    res.json(product);
-  })
-);
+router.route("/:id").get(getProductById);
 
 export default router;
