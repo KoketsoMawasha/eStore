80f4026bf60b416015ac2b482d3b8c4a7d473b68
Koketso Mawasha <Koketsomawasha@gmail.com>
2021-07-21 23:02:01 +0200

    User registration and password encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -28,6 +28,35 @@ const authUser = async (req, res) => {
   }
 };
 
+// @desc    REGISTER NEW USER
+// @route   POST /api/users/
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { email, password, name } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({ name, email, password });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("invalid user data");
+  }
+});
+
 // @desc    Get user profile
 // @route   GET /api/users/profile
 // @access  Private
@@ -48,4 +77,4 @@ const getUserProfile = (req, res) => {
   }
 };
 
-export { authUser, getUserProfile };
+export { authUser, getUserProfile , registerUser};
