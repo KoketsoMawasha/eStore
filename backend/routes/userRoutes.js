46118822c164c514049f7ef9843473957a1f8aac
Koketso Mawasha <Koketsomawasha@gmail.com>
2021-08-02 17:24:50 +0200

    update profile endpoint implementation

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,14 +4,19 @@ import {
   authUser,
   getUserProfile,
   registerUser,
+  updateUserProfile,
 } from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 router.route("/").post(registerUser);
 router.post("/login", authUser);
-router.get("/", (req, res) => {});
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+
+// router.get("/", (req, res) => {});
 // api/users/profile
-router.get("/profile", protect, getUserProfile);
 //router.route("profile").get(protect, getUserProfile);
 
 export default router;
