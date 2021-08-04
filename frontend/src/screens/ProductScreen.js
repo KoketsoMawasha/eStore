9d3f032b21755235ca78662dfcd99c1bc909da44
Koketso Mawasha <Koketsomawasha@gmail.com>
2021-08-04 12:37:11 +0200

    Shipping screen and checkout steps component

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -15,6 +15,7 @@ import { getProductDetail } from "../actions/productActions";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 
+
 const ProductScreen = ({ history, match }) => {
   const [qty, setQty] = useState(1);
   const dispatch = useDispatch();
